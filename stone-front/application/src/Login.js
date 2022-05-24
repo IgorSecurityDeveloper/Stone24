375578bc03891870ddc82748c48a5b3275ad8879
Igor <igoramarals96@gmail.com>
2022-05-24 01:27:29 -0300

    Validação de email e senha corrigido

--- stone-front/application/src/Login.js
+++ stone-front/application/src/Login.js
@@ -1,29 +1,15 @@
 import React, { useState } from 'react';
+// import Credenciais from "./stone-back/ValidarLogin";
 import './login.css';
-// import { email, password } from "./stone-back/Eventos/validarLogin.js";
 
 import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
 /* Preencher com uma página de recuperação de senha no link de recuperação de senha */
 
+var MensagemERRO = `<div className="recuperarSenha">Login ou Senha Incorretos, por favor tente novamente!</div>`;
+
 function Login() {
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
   const [show, setShow] = useState(false)
-
-  function Credenciais(email, password) {
-   var senha = document.querySelector(password)
-   var usuario = document.querySelector(email)
-
-    if (senha === "123456" && usuario === "igor@stone.com.br") {
-      window.alert("Teste: Essa senha está certa!")
-    }
-    else {
-      window.alert("Teste: Email ou senha incorretos!")
-    }
-  }
-
-//  Credenciais() 
 
   function handleClick(e) {
     setShow(!show)
@@ -42,8 +28,6 @@ function Login() {
             className="campologin"
             type="email"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
             method="POST"
             placeholder="Login"
             required
@@ -54,8 +38,6 @@ function Login() {
             className="camposenha"
             type={show ? "text" : "password"}
             id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
             method="POST"
             placeholder="Senha"
             required
@@ -63,31 +45,33 @@ function Login() {
           <hr className="line2"></hr>
 
           <div className="icone-olho">
-            {show ?
-              (<AiOutlineEye
-                size={30}
-                onClick={handleClick}
-              />
-              ) :
-              (
-                <AiOutlineEyeInvisible
-                  size={30}
-                  onCLick={handleClick}
-                />
-              )
-            }
+            {show ? (<AiOutlineEye size={30} onClick={handleClick} />) : (<AiOutlineEyeInvisible size={30} onCLick={handleClick} />)}
           </div>
 
+          <div className="ErrDiv">  </div>
 
           <a className="recuperarSenha" link href="http://">Esqueceu a senha</a>
 
-          <button className="EnviarLogin" method="POST" type="submit" onSubmit={Credenciais()}>Confirmar</button>
-        </form>
-      </div>
+          <button className="EnviarLogin" method="POST" type="submit" onClick={
 
+//Função credenciais corrigida, verificar forma de importar ValidarLogin de maneira correta no React
+            function Credenciais() {
+              var senha = document.getElementById('password')
+              var usuario = document.getElementById('email')
+
+              if (usuario.value == "igor@stone.com.br" && senha.value == 123456) {
+                window.alert("Teste: Essas credenciais estão corretas!")
+              }
+              else {
+                window.alert("Teste: Email ou senha incorretos!")
+              }
+          }
+          }> Confirmar </button>
+
+      </form>
     </div>
 
-
+    </div>
 
   );
 }
