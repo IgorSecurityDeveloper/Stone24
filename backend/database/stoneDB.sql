ed832b9dbce048ae641dcdd51524c86c60c0faaf
Igor <igoramarals96@gmail.com>
2023-03-26 20:19:34 -0300

    Created first EDR
    stoneDB_ERD to understand better the project

--- backend/database/stoneDB.sql
+++ backend/database/stoneDB.sql
@@ -1,12 +1,54 @@
-CREATE DATABASE IF NOT EXIST StoneDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE
+    IF NOT EXISTS StoneDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 USE StoneDB;
 
-CREATE TABLE IF NOT EXISTS ticket (
-ticketNumber int(8),
-ticketType varchar(20),
-priority varchar(20),
-impact varchar(20),
-contactMethod varchar(10),
-primary key ticketNumber
-);
+CREATE TABLE
+    IF NOT EXISTS ticket (
+        ticketNumber int(8) NOT NULL AUTO_INCREMENT,
+        ticketType varchar(20) NOT NULL,
+        priority_status varchar(20) NOT NULL,
+        impact varchar(20) NOT NULL,
+        contactMethod varchar(10) NOT NULL,
+        userId int(10) NOT NULL,
+        primary key (ticketNumber),
+        foreign key (userId) references user(userID)
+    );
 
+CREATE TABLE
+    IF NOT EXISTS user (
+        userID int(10) NOT NULL AUTO_INCREMENT,
+        userName varchar(255) NOT NULL,
+        authCode int(6) NOT NULL,
+        address varchar(255) NOT NULL,
+        language varchar(255),
+        password varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
+        role varchar(255) NOT NULL,
+        userActive boolean NOT NULL,
+        primary key (userID)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS analyst (
+        analystID int(10) NOT NULL AUTO_INCREMENT,
+        analystUserName varchar(255) NOT NULL,
+        authCode int(6) NOT NULL,
+        address varchar(255) NOT NULL,
+        language varchar(255),
+        password varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
+        role varchar(255) NOT NULL,
+        userActive boolean NOT NULL,
+        assignedTicket varchar(255),
+        PRIMARY KEY (analystID)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS group_members (
+        groupName varchar(255) NOT NULL,
+        groupDescription varchar(255),
+        groupMembers varchar(255),
+        userId int(10) NOT NULL,
+        Foreign Key (userId) references user(userID)
+    );
